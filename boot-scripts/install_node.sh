ed8ed0aa1ee58164c266630f06912e85e975835c
Adam Taylor <adam@appsorcery.com>
2016-04-07 23:24:13 -0700

    blah

--- boot-scripts/install_node.sh
+++ boot-scripts/install_node.sh
@@ -13,16 +13,22 @@ ln -s /usr/local/bin/sails /usr/bin/sails
 ln -s /usr/local/bin/forever /usr/bin/forever
 ln -s /usr/local/bin/forever-service /usr/bin/forever-service
 
-#sudo npm install
+mkdir -p /var/app/current
+cd /var/app/current
 
-#aws configure?
-cd
-aws s3 --region us-east-1 cp s3://cdn.eelrb.com/www/prod/app.js .tmp/prerender/
-aws s3 --region us-east-1 cp s3://cdn.eelrb.com/www/prod/app.manifest.json .tmp/public/
-aws s3 --region us-east-1 cp s3://cdn.eelrb.com/www/prod/app.chunk-manifest.json .tmp/public/
+aws s3 --region us-east-1 cp s3://cdn.eelrb.com/www/dev/dev.zip .
+aws s3 --region us-east-1 cp s3://cdn.eelrb.com/www/dev/app.js .tmp/prerender/
+aws s3 --region us-east-1 cp s3://cdn.eelrb.com/www/dev/app.manifest.json .tmp/public/
+aws s3 --region us-east-1 cp s3://cdn.eelrb.com/www/dev/app.chunk-manifest.json .tmp/public/
       
-aws s3 --region us-east-1 cp s3://cdn.eelrb.com/www/prod/app_public.js .tmp/prerender/
-aws s3 --region us-east-1 cp s3://cdn.eelrb.com/www/prod/app_public.manifest.json .tmp/public/
-aws s3 --region us-east-1 cp s3://cdn.eelrb.com/www/prod/app_public.chunk-manifest.json .tmp/public/
+aws s3 --region us-east-1 cp s3://cdn.eelrb.com/www/dev/app_public.js .tmp/prerender/
+aws s3 --region us-east-1 cp s3://cdn.eelrb.com/www/dev/app_public.manifest.json .tmp/public/
+aws s3 --region us-east-1 cp s3://cdn.eelrb.com/www/dev/app_public.chunk-manifest.json .tmp/public/
       
-aws s3 --region us-east-1 cp s3://cdn.eelrb.com/www/prod/i18n .tmp/prerender/i18n/ --recursive
+aws s3 --region us-east-1 cp s3://cdn.eelrb.com/www/dev/i18n .tmp/prerender/i18n/ --recursive
+
+unzip dev.zip
+tar -xvf dev.tar
+npm install --production
+
+#forever-service install dev2

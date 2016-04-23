3edbe80ed499bfc937d31b51f92888f1afdda3aa
Adam Taylor <adam@appsorcery.com>
2016-04-23 10:24:35 -0700

    absolute path for forever service

--- boot-scripts/setup_forever.sh
+++ boot-scripts/setup_forever.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-echo "forever-service install $APP_NAME --script app.js -o \" $APP_STARTUP_ARGS\""
-forever-service install $APP_NAME --script app.js -o " $APP_STARTUP_ARGS"
+echo "forever-service install $APP_NAME --script /var/app/current/app.js -o \" $APP_STARTUP_ARGS\""
+forever-service install $APP_NAME --script /var/app/current/app.js -o " $APP_STARTUP_ARGS"
 service $APP_NAME start
